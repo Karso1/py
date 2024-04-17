8cb988b9472c0cd70b94285a6d4d38c27734537d
karsol <129362623+Karso1@users.noreply.github.com>
2024-04-17 14:07:40 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,33 @@
+"""
+there are  five :numbers,int,long,float,complex
+list
+tuple
+string
+dictionary
+
+"""
+
+#switch the data
+print(float(2))
+print(int(2.8))
+
+
+#+-*/
+print(10+4)
+print(10-4)
+print(10*4)
+print(10/4)
+print(10%4)
+
+
+#list
+empty_list = []
+empty_list = list()
+instruments = ['600010.SHA','000001.SZA','300010.SZA']
+instruments
+
+print(len(instruments))
+print(instruments[0])
+print(instruments[-1])
+print(instruments[2])
+
